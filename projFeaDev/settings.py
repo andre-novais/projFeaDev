7f475ee369a7f1ac7216dfb979dd5e05d6f4d025
andre <andrenovais.git@gmail.com>
2019-11-15 17:38:24 -0300

    3

--- projFeaDev/settings.py
+++ projFeaDev/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+ #   os.path.join(PROJECT_DIR, 'static'),
+#)
+STATICFILES_DIRS=('. /static',)
+
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
